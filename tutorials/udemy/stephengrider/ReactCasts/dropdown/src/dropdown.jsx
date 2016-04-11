0320eb13aa899cad7989146d27add8fe1f3417a1
Alexander <awsaavedra@gmail.com>
2016-04-10 20:16:21 -0700

    exploring state and events - toggling

--- tutorials/udemy/stephengrider/ReactCasts/dropdown/src/dropdown.jsx
+++ tutorials/udemy/stephengrider/ReactCasts/dropdown/src/dropdown.jsx
@@ -6,9 +6,14 @@ var Button = require('./button');
 var ListItem = require('./list-item');
 
 module.exports = React.createClass({
-
+  handleClick: function(){
+    //never directly manipulate state obj e.g. this.state.open = false;
+    this.setState({open: !this.state.open});
+  },
+  getInitialState: function(){
+    return { open: false }
+  },
   render: function(){
-
     var list = this.props.items.map(function(item){
       return <ListItem item = {item} />
     });
@@ -20,7 +25,7 @@ module.exports = React.createClass({
         title = {this.props.title}
         subTitleClassName = "caret"
         />
-        <ul>
+        <ul className = {"dropdown-menu " + (this.state.open ? "show": "")}>
           {list}
         </ul>
     </div>
