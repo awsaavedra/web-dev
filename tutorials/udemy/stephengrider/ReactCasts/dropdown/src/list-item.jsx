92f835b99932c21241aea6379d51cd46eff1ecd5
Alexander <awsaavedra@gmail.com>
2016-04-12 14:20:36 -0700

    exploring state and events - selecting items

--- tutorials/udemy/stephengrider/ReactCasts/dropdown/src/list-item.jsx
+++ tutorials/udemy/stephengrider/ReactCasts/dropdown/src/list-item.jsx
@@ -1,7 +1,12 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  handleClick: function(){
+    this.props.whenItemClicked(this.props.item); //callback function
+  },
   render: function(){
-      return <li> <a>{this.props.item}</a></li>
+      return <li className = {this.props.className}>
+        <a onClick = {this.handleClick}>{this.props.item}</a>
+      </li>
   }
 });
