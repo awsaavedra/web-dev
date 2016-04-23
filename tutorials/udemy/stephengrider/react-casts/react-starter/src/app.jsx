1328082b1c5f2dabe7e43ad3aaeadf9c4db6beac
Alexander <awsaavedra@gmail.com>
2016-04-23 16:41:00 -0700

    up to handling inputs

--- tutorials/udemy/stephengrider/react-casts/react-starter/src/app.jsx
+++ tutorials/udemy/stephengrider/react-casts/react-starter/src/app.jsx
@@ -2,23 +2,27 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
+var Header = require('./header');
 var rootUrl = 'https://alexander-todo.firebaseio.com/';
 
-
 var Hello = React.createClass({
-  mixins: [ ReactFire ],
-  componentWillMount: function(){
+  mixins: [ ReactFire],
+
+  componentWillMount: function() {
     this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
   },
+
   render: function() {
-    console.log(this.state);
-    return <h1 className = "red">
-      Hello, React!
-    </h1>
+    return <div className = "row panel panel-default">
+      <div className = "col-md-8 col-md-offset-2">
+        <h2 className = "text-center">
+          To-Do List
+        </h2>
+        <Header />
+      </div>
+    </div>
   }
 });
 
 var element = React.createElement(Hello, {});
 ReactDOM.render(element, document.querySelector('.container'));
-
-//var Firebase = require('reactfire/node_modules/firebase');
